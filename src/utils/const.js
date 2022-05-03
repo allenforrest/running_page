6c6abb827183b5de89b2fb1af0ca07f8554c3e07
Allen Xu <xuweinan@akey.me>
2022-05-03 10:29:05 +0800

    update my owned mapbox token

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,6 +1,7 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoiYWxsZW54ODEyIiwiYSI6ImNsMnBqMWlwNjJsZmszY2x3djRrMDYwa3MifQ.qN-AMnjRcWKEn6maVmyqfQ';
+  //'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
